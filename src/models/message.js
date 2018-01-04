5fb0d30f806924243b068a926614dedb3125fda0
Uri <Urikutner@gmail.com>
2018-01-04 15:11:45 +0200

    improve id generator for individual messages

--- src/models/message.js
+++ src/models/message.js
@@ -1,11 +1,14 @@
-
-
-var uniqueId = 0;
-
 export default class Message {
 	constructor(username, content){
 		this.username = username;
 		this.content = content;
-		this.id = uniqueId++;
+		this.id = generateUniqueId();
+
+		function generateUniqueId(){
+			var t = new Date().getTime();
+			var r = Math.floor(Math.random() * 100000);
+	
+			return t + "" + r;
+		}
 	}
 }
